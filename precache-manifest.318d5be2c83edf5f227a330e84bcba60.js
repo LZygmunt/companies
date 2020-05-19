834a97ffc52dd3f1810d1133e0a4a56104cb80a7
Lzygmunt <lzygmunt0@gmail.com>
2020-05-19 12:18:18 +0200

    Updates

---
+++ precache-manifest.318d5be2c83edf5f227a330e84bcba60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16c2861213b39dc6f574e07acf1576ee",
+    "url": "/companies/index.html"
+  },
+  {
+    "revision": "0f089edcd3052b958fd4",
+    "url": "/companies/static/css/main.7dff6d4e.chunk.css"
+  },
+  {
+    "revision": "a00de5334add04d6c79b",
+    "url": "/companies/static/js/2.4f56b265.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/companies/static/js/2.4f56b265.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f089edcd3052b958fd4",
+    "url": "/companies/static/js/main.3eed83e6.chunk.js"
+  },
+  {
+    "revision": "8c5f20b94edf6af320f2",
+    "url": "/companies/static/js/runtime-main.13331fa0.js"
+  }
+]);
